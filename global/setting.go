f78352c63056c948ef728ea4d1c0da65ca9f1a47
tupig-7 <2664006323@qq.com>
2021-10-20 21:29:46 +0800

    公共模块

---
+++ global/setting.go
@@ -0,0 +1,13 @@
+package global
+
+import (
+	"baozi-server/pkg/logger"
+	"baozi-server/pkg/setting"
+)
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+	Logger          *logger.Logger
+)
